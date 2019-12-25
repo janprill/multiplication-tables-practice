9529c42c0353c83467d535eda66ecea756491883
Jan Prill <Jan.Prill@mspag.com>
2019-12-25 19:19:50 +0100

    Germification

--- multiplication.sh
+++ multiplication.sh
@@ -3,13 +3,13 @@
 
 errors=0
 num=20
-question_str="product"
+question_str="Malgenommen"
 random_range=100
 result=-1
 
 # Create an array of 100 multiplication questions and answers as a default.
-for j in $( seq 1 10); do 
-    for i in $( seq 1 10); do 
+for j in $( seq 1 10); do
+    for i in $( seq 1 10); do
         questions[((element++))]="$i x $j=$(($i*$j))"
     done
 done
@@ -19,31 +19,31 @@ while getopts 'dasq:' OPTION; do
     case "$OPTION" in
     d) # Division
         questions=() # Clear questions array
-        element=0 
-        question_str="division"
-        for j in $( seq 1 10); do 
-            for i in $( seq 1 10); do 
+        element=0
+        question_str="Geteilt durch"
+        for j in $( seq 1 10); do
+            for i in $( seq 1 10); do
                 questions[((element++))]="$(($i*$j)) : $j=$(($(($i*$j))/$j))"
             done
         done
         ;;
     a) # Addition
         questions=() # Clear questions array
-        element=0 
-        question_str="sum"
-        for j in $( seq 1 10); do 
-            for i in $( seq 1 10); do 
+        element=0
+        question_str="Addition"
+        for j in $( seq 1 10); do
+            for i in $( seq 1 10); do
                 questions[((element++))]="$i + $j=$(($i+$j))"
             done
         done
         ;;
     s) # Subtraction
         questions=() # Clear questions array
-        element=0 
-        question_str="result"
+        element=0
+        question_str="Ergebnis"
         random_range=55
-        for j in $( seq 1 10); do 
-            for i in $( seq 1 10); do 
+        for j in $( seq 1 10); do
+            for i in $( seq 1 10); do
                 item=$( echo "$i - $j=$(($i-$j))" | grep -v "=-")
                 if [ ! -z "$item" ]; then
                     questions[((element++))]=$item
@@ -52,10 +52,10 @@ while getopts 'dasq:' OPTION; do
         done
         ;;
     q)
-        num=$OPTARG 
+        num=$OPTARG
         ;;
     ?)
-        echo "script usage: $(basename $0) [-d] [-q total_questios]" >&2
+        echo "script usage: $(basename $0) [-d] [-q total_questions]" >&2
         exit 1
         ;;
     esac
@@ -75,37 +75,37 @@ function get_question {
 function print_question {
 
     echo "################################"
-    printf "\033[0;36mWhat is the $question_str of $question ?\e[0m\n"
-    echo -n "Your answer: "
+    printf "\033[0;36mWas ist das $question_str von $question ?\e[0m\n"
+    echo -n "Deine Antwort: "
 }
 
 # A core function to ask a question and compare response with a valid result.
 function ask_question {
 
 response=0
-while [ $response -ne $result ]; do 
+while [ $response -ne $result ]; do
 
-    
+
     print_question
     read response
-    
+
     # Keep asking for a response until we get a valid integer.
     while [[ $((response)) != $response ]]; do
         print_question
         read response
     done
-    
+
 
     if [ $response -eq $result ]; then
-        printf "\033[1;32mCorrect !!!\e[0m\n"
+        printf "\033[1;33mKorrekt !!!\e[0m\n"
         num=$[$num-1]
-        printf "\033[0;33mRemaining questions: $num \e[0m\n"
+        printf "\033[0;33mVerbleibende Fragen: $num \e[0m\n"
     else
-        printf "\033[1;31mWrong answer, try again !!!\e[0m\n"
+        printf "\033[1;31mFalsche Antwort, versuche es nochmal !!!\e[0m\n"
         errors=$[$errors+1]
-        printf "\033[0;33mRemaining questions: $num \e[0m\n"
+        printf "\033[0;33mVerbleibende Fragen: $num \e[0m\n"
     fi
-    
+
 done
 
 }
@@ -116,4 +116,4 @@ until [  $num -eq 0 ]; do
 done
 
 # echo "Congratulations, your practice test is finished!!!"
-echo "Wrong answers: $errors"
+echo "Falsche Antworten: $errors"
